9165881c4f905e1c0333273e60abfeb9b5413647
Chris LaRose <chris.larose@mycase.com>
2015-12-14 22:11:46 -0800

    cjl - Finish day 2

--- src/advent/core.clj
+++ src/advent/core.clj
@@ -19,19 +19,35 @@
                (first remaining)
                (rest remaining))))))
 
-(defn day2-wrapping-paper [input]
+(defn day2-parse [input]
   (let [parse-d (fn [d] (->> (split d #"x")
-                             (mapv #(. Integer parseInt %))))
-        all-dimensions (->> (split input #"\n")
-                            (map parse-d))
-        surface-area (fn [[l w h]]
+                             (mapv #(. Integer parseInt %))))]
+    (->> (split input #"\n")
+         (map parse-d))))
+
+(defn day2-wrapping-paper [input]
+  (let [surface-area (fn [[l w h]]
                        (* 2 (+ (* l w) (* w h) (* h l))))
         area-smallest-face (fn [[l w h]]
                              (min (* l w) (* w h) (* h l)))
         wrapping-paper (fn [ds]
                          (+ (surface-area ds) (area-smallest-face ds)))]
-    (->> all-dimensions
+    (->> input
+         (day2-parse)
          (map wrapping-paper)
+         (reduce + 0))))
+
+(defn day2-ribbon [input]
+  (let [p (fn [[w h]] (+ (* 2 w) (* 2 h)))
+        shortest-perimeter (fn [[l w h]]
+                             (min (p [l w]) (p [w h]) (p [h l])))
+        volume (fn [[l w h]]
+                 (* l w h))
+        ribbon (fn [ds]
+                 (+ (shortest-perimeter ds) (volume ds)))]
+    (->> input
+         (day2-parse)
+         (map ribbon)
          (reduce + 0))))
 
 (defn -main
