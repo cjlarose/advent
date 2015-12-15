4cd1a948d6b377754e68423ec25f366fcdc16004
Chris LaRose <chris.larose@mycase.com>
2015-12-14 23:03:55 -0800

    cjl - Add day4

---
+++ src/advent/day4.clj
@@ -0,0 +1,17 @@
+(ns advent.day4)
+
+(defn md5 [token]
+  (let [hash-bytes (doto (java.security.MessageDigest/getInstance "MD5")
+                     (.reset)
+                     (.update (.getBytes token)))
+        digest (.digest hash-bytes)
+        bi (java.math.BigInteger. 1 digest)]
+    (format (str "%0" (* 2 (count digest)) "X") bi)))
+
+(defn find-hash [k]
+  (loop [i 1]
+    (let [input (str k i)
+          h     (md5 input)]
+      (if (every? #(= % \0) (take 5 h))
+        i
+        (recur (inc i))))))
