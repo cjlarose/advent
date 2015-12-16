7b74c05786b989027392816402191db4b27f1f8e
Chris LaRose <chris.larose@mycase.com>
2015-12-15 20:21:53 -0800

    cjl - Finish day 6

--- src/advent/day6.clj
+++ src/advent/day6.clj
@@ -9,12 +9,16 @@
          (re-matches #"(.*) ([\d,]+) through ([\d,]+)")
          (read-parts))))
 
+(defn parse [input]
+  (->> (split input #"\n")
+       (map parse-command)))
+
 (defn houses-in-block [[start-i start-j] [end-i end-j]]
   (set (for [i (range start-i (inc end-i))
              j (range start-j (inc end-j))]
          [i j])))
 
-(defn handle-command [lit-houses [instruction start end]]
+(defn flip-lights [lit-houses [instruction start end]]
   (let [affected-houses (houses-in-block start end)
         [turn-on turn-off] (case instruction
                              "turn on" [affected-houses #{}]
@@ -26,7 +30,26 @@
         (union turn-on))))
 
 (defn count-lit-homes [input]
-  (->> (split input #"\n")
-       (map parse-command)
-       (reduce handle-command #{})
+  (->> input
+       (parse)
+       (reduce flip-lights #{})
        (count)))
+
+(defn change-brightness [brightnesses [instruction start end]]
+  (let [affected-houses (houses-in-block start end)
+        change-f (case instruction
+                   "turn on" inc
+                   "turn off" #(max 0 (dec %))
+                   "toggle" (partial + 2))
+        handle-home (fn [state home]
+                      (let [v (or (state home) 0)]
+                        (assoc state home (change-f v))))]
+    (->> (houses-in-block start end)
+         (reduce handle-home brightnesses))))
+
+(defn total-brightness [input]
+  (->> input
+       (parse)
+       (reduce change-brightness {})
+       (vals)
+       (reduce + 0)))
